16fb757f91f5b6b6aa64ae55ae0cb89a5ffd4744
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-12 18:06:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil 
   lowest_key = nil 
   name_hash.each do |name, value|
-    if value < lowest_value
+    if lowest_value value < lowest_value
       lowest_value = v 
       lowest_key = k 
     end
